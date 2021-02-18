21b62edb9cdc4a6d98c88f2311790c3f97d929f8
Richard Leek <richard@richardleek.com>
2021-02-18 17:32:08 -0500

    make sure packet length is valid

--- src/client.rs
+++ src/client.rs
@@ -2,7 +2,7 @@ use std::{io::Read, net::TcpStream};
 
 use eo::{
     data::EOByte,
-    net::{ClientState, PacketProcessor},
+    net::{ClientState, PACKET_LENGTH_SIZE, PacketProcessor},
 };
 
 pub struct Client {
@@ -26,21 +26,30 @@ impl Client {
     }
 
     fn receive_and_process(&mut self) -> std::io::Result<()> {
-        let length_buf = self.receive(2)?;
-        let data_length = eo::data::decode_number(&length_buf) as usize;
-        let mut data_buf = self.receive(data_length)?;
-        self.processor.decode(&mut data_buf);
+        let data_length = self.get_packet_length()?;
+        if data_length > 0 {
+            let mut data_buf = self.receive(data_length)?;
+            self.processor.decode(&mut data_buf);
 
-        if data_buf.len() > 0 {
-            debug!("Recv: {:?}", data_buf);
+            if data_buf.len() > 0 {
+                debug!("Recv: {:?}", data_buf);
+            }
         }
 
         Ok(())
     }
 
+    fn get_packet_length(&mut self) -> std::io::Result<usize> {
+        let length_buf = self.receive(PACKET_LENGTH_SIZE)?;
+        Ok(eo::data::decode_number(&length_buf) as usize)
+    }
+
     fn receive(&mut self, length: usize) -> std::io::Result<Vec<EOByte>> {
         let mut buf: Vec<EOByte> = vec![0; length];
-        self.stream.read_exact(&mut buf)?;
+        match self.stream.read_exact(&mut buf) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
         Ok(buf)
     }
 }

f50c09568bdee732ffa44c14b8642e08726abbf7
Richard Leek <richard@richardleek.com>
2023-01-13 17:49:54 -0500

    use official distance calculations

--- src/utils/in_range.rs
+++ src/utils/in_range.rs
@@ -4,9 +4,13 @@ pub fn in_range(x1: f64, y1: f64, x2: f64, y2: f64) -> bool {
     in_range_distance(x1, y1, x2, y2, SETTINGS.world.see_distance as f64)
 }
 
-pub fn in_range_distance(x1: f64, y1: f64, x2: f64, y2: f64, distance: f64) -> bool {
-    let dx = x1 - x2;
-    let dy = y1 - y2;
-    let actual_distance = ((dx * dx) + (dy * dy)).sqrt();
-    actual_distance.abs() <= distance
+pub fn in_range_distance(x1: f64, y1: f64, x2: f64, y2: f64, _distance: f64) -> bool {
+    let dx = (x1 - x2).abs();
+    let dy = (y1 - y2).abs();
+
+    if x1 > x2 || y1 > y2 {
+        dx + dy <= 12.0
+    } else {
+        dx + dy <= 15.0
+    }
 }

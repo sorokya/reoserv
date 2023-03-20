2c5e691bc5d56f5517455339249f8442c0b2d39e
Richard Leek <richard@richardleek.com>
2023-03-20 18:21:28 +0100

    optimize is_occupied

--- src/map/is_occupied.rs
+++ src/map/is_occupied.rs
@@ -1,6 +1,5 @@
 use eo::protocol::Coords;
 
-pub fn is_occupied(coords: Coords, occupied_tiles: &Vec<Coords>) -> bool {
-    // idk if this is ref check or val check
+pub fn is_occupied(coords: Coords, occupied_tiles: &[Coords]) -> bool {
     occupied_tiles.contains(&coords)
 }

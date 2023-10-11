2932320abd94ec5a4fbd0f6dce9fd867c8605d32
Richard Leek <richard@richardleek.com>
2023-10-11 10:00:15 +0200

    fix exp boost

--- src/map/map/give_experience.rs
+++ src/map/map/give_experience.rs
@@ -1,12 +1,15 @@
 use eo::data::{EOInt, EOShort};
 
+use crate::SETTINGS;
+
 use super::Map;
 
 impl Map {
     pub fn give_experience(&mut self, player_id: EOShort, experience: EOInt) -> (bool, EOInt) {
         match self.characters.get_mut(&player_id) {
             Some(character) => {
-                let leveled_up = character.add_experience(experience);
+                let leveled_up =
+                    character.add_experience(experience * SETTINGS.world.exp_multiplier);
                 (leveled_up, character.experience)
             }
             None => (false, 0),

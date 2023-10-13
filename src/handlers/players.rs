0a171105b0b693237a07f3a5b990a5590442f7ec
Richard Leek <richard@richardleek.com>
2023-10-13 19:18:56 +0200

    friends list online status

--- src/handlers/players.rs
+++ src/handlers/players.rs
@@ -1,14 +1,14 @@
 use eo::{
     data::{Serializeable, StreamBuilder, StreamReader},
     protocol::{
-        server::init::{Init, InitData, InitPlayers},
+        server::init::{Init, InitData, InitFriendListPlayers, InitPlayers},
         InitReply, PacketAction, PacketFamily,
     },
 };
 
 use crate::{player::PlayerHandle, world::WorldHandle};
 
-pub async fn list(player: PlayerHandle, world: WorldHandle) {
+pub async fn request(player: PlayerHandle, world: WorldHandle) {
     let players = world.get_online_list().await;
 
     let reply = Init {
@@ -24,6 +24,22 @@ pub async fn list(player: PlayerHandle, world: WorldHandle) {
     player.send(PacketAction::Init, PacketFamily::Init, builder.get());
 }
 
+pub async fn list(player: PlayerHandle, world: WorldHandle) {
+    let players = world.get_online_list().await;
+
+    let reply = Init {
+        reply_code: InitReply::FriendListPlayers,
+        data: InitData::FriendListPlayers(InitFriendListPlayers {
+            num_online: players.len() as u16,
+            list: players.iter().map(|p| p.name.to_owned()).collect(),
+        }),
+    };
+
+    let mut builder = StreamBuilder::new();
+    reply.serialize(&mut builder);
+    player.send(PacketAction::Init, PacketFamily::Init, builder.get());
+}
+
 pub async fn players(
     action: PacketAction,
     _reader: StreamReader,
@@ -31,7 +47,8 @@ pub async fn players(
     world: WorldHandle,
 ) {
     match action {
-        PacketAction::Request => list(player, world).await,
+        PacketAction::Request => request(player, world).await,
+        PacketAction::List => list(player, world).await,
         _ => error!("Unhandled packet Players_{:?}", action),
     }
 }

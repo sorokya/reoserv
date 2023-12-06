4a2d268ae4ec5575c6dd65f4b87fa784f154e427
Richard Leek <richard@richardleek.com>
2023-12-06 22:01:39 +0100

    Implement mute, freeze, unfreeze

--- src/world/world/admin/mute_player.rs
+++ src/world/world/admin/mute_player.rs
@@ -1,5 +1,33 @@
+use eo::{
+    data::StreamBuilder,
+    protocol::{PacketAction, PacketFamily},
+};
+
+use crate::LANG;
+
 use super::super::World;
 
 impl World {
-    pub fn mute_player(&mut self, victim_name: String, admin_name: String) {}
+    // TODO: Server side mute?
+    pub fn mute_player(&mut self, victim_name: String, admin_name: String) {
+        let player_id = match self.characters.get(&victim_name) {
+            Some(player_id) => player_id,
+            None => return,
+        };
+
+        let player = match self.players.get(player_id) {
+            Some(player) => player,
+            None => return,
+        };
+
+        let mut builder = StreamBuilder::new();
+        builder.add_string(&admin_name);
+        player.send(PacketAction::Spec, PacketFamily::Talk, builder.get());
+
+        self.broadcast_server_message(&get_lang_string!(
+            &LANG.announce_mute,
+            victim = victim_name,
+            name = admin_name
+        ));
+    }
 }
--- src/world/world/admin/unfreeze_player.rs
+++ src/world/world/admin/unfreeze_player.rs
@@ -1,5 +1,32 @@
+use bytes::Bytes;
+use eo::protocol::{PacketAction, PacketFamily};
+
+use crate::LANG;
+
 use super::super::World;
 
 impl World {
-    pub fn unfreeze_player(&mut self, victim_name: String, admin_name: String) {}
+    pub fn unfreeze_player(&mut self, victim_name: String, admin_name: String) {
+        let player_id = match self.characters.get(&victim_name) {
+            Some(player_id) => player_id,
+            None => return,
+        };
+
+        let player = match self.players.get(player_id) {
+            Some(player) => player,
+            None => return,
+        };
+
+        player.send(
+            PacketAction::Open,
+            PacketFamily::Walk,
+            Bytes::from_static(b"S"),
+        );
+
+        self.broadcast_server_message(&get_lang_string!(
+            &LANG.announce_unfreeze,
+            victim = victim_name,
+            name = admin_name
+        ));
+    }
 }

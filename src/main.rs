65f96a1407803196f42f6b3bab1dd123ec5d3107
Richard Leek <richard@richardleek.com>
2025-04-30 04:29:22 +0200

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const VERSION: &str = "1.9.0";
+const VERSION: &str = "1.10.0";
 
 // Avoid musl's default allocator due to lackluster performance
 // https://nickb.dev/blog/default-musl-allocator-considered-harmful-to-performance
